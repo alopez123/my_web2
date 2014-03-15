44716902e0d6456fc716c38e6f038dc9ea587d20
adr14nmxc <adr14nmxc@adr14nmxc-desktop>
2014-03-15 14:36:10 -0500

    Primera modificacion probando github

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html long="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="stylesheet2.css">
+        <title></title>
+    </head>
+    <body>
+        <div id="contenido">
+            <header>
+                <hgroup>
+                    <h1> Pagina de prueba</h1> 
+ 
+                </hgroup>  
+                <nav>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Tutoriales</li>
+                    <li>Contacto</li>
+                    <li>Media</li>
+                </ul>  
+            </nav>
+                </header>  
+            <section>
+                <div id="textopr">
+                    <article>
+                        <hgroup>
+                            <h1>Bienvenido a la pagina principal</h1>    
+                        </hgroup>    
+                        <p>HTML5 (HyperText Markup Language, versión 5) es la quinta revisión importante del lenguaje básico de la World Wide Web, HTML. HTML5 especifica dos variantes de sintaxis para HTML: un «clásico» HTML (text/html), la variante conocida como HTML5 y una variante XHTML conocida como sintaxis XHTML5 que deberá ser servida como XML</p>    
+                        
+                    </article> 
+                    <aside>
+                        <p>Visita nuestro canal de youtube</p>
+                        <p>Siguenos en facebook <a href="">ALGWEB</a></p>
+                        
+                    </aside>
+                    
+                </div>  
+                
+            </section>   
+            
+        </div>      
+        
+        
+    </body>
+</html>
